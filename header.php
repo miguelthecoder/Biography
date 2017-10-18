802b8ded1499ddcf6e1ea41fe7403788b994c23c
Miguel Rosas <miguelrosas@Miguels-Mac.local>
2017-10-18 16:32:27 -0400

    fixed image

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 		<nav>
 			<ul>
 				<li><a href="index.php">Home</a></li>
-				<li><a href="biography.php">Biography</a></li>
+				<li><a href="biography.php">Bio</a></li>
 				<li><a href="projects.php">Projects</a></li>
 				<li><a href="resume.php">Resume</a></li>
 				<li><a href="contact.php">Contact</a></li>
