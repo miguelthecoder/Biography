2ed9e2593d585bf58a3a755ec2e2378472a0a8f3
Miguel Rosas <miguelrosas@Miguels-Mac.local>
2017-10-25 01:34:35 -0400

    fixed email

--- contact.php
+++ contact.php
@@ -39,27 +39,25 @@ include 'header.php';
 
 	    function died($error) {
 	        // your error code can go here
-	        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-	        echo "These errors appear below.<br /><br />";
-	        echo $error."<br /><br />";
-	        echo "Please go back and fix these errors.<br /><br />";
+	        echo $error;
+	        echo "Sorry there was an error";
 	        die();
 	    }
 
 
 	    // validation expected data exists
-	    if(!isset($_POST['first_name']) ||
-	        !isset($_POST['last_name']) ||
+	    if(!isset($_POST['firstName']) ||
+	        !isset($_POST['lastName']) ||
 	        !isset($_POST['email']) ||
-	        !isset($_POST['telephone']) ||
+	        !isset($_POST['phone']) ||
 	        !isset($_POST['comments'])) {
-	        died('We are sorry, but there appears to be a problem with the form you submitted.');
+	        died('Opps, something went wrong.');
 	    }
 
 
 
-	    $first_name = $_POST['first_name']; // required
-	    $last_name = $_POST['last_name']; // required
+	    $first_name = $_POST['firstName']; // required
+	    $last_name = $_POST['lastName']; // required
 	    $email_from = $_POST['email']; // required
 	    $telephone = $_POST['phone']; // not required
 	    $comments = $_POST['comments']; // required
@@ -114,7 +112,7 @@ include 'header.php';
 
 	<!-- include your own success html here -->
 
-	Thank you for contacting us. We will be in touch with you very soon.
+	<p>Thank you for contacting me. I will be in touch with you very soon.</p>
 
 	<?php
 
