432021e4ba52dee2e2f0a0e8c58c643c07ac0fca
Miguel Rosas <miguelrosas@Miguels-Mac.local>
2017-10-25 01:23:37 -0400

    fixed content

--- contact.php
+++ contact.php
@@ -10,10 +10,10 @@ include 'header.php';
 				<div class="forms">
 					<label for="firstName">First Name:
 						<!-- ///minlegth attribute requires a minimun length  -->
-						<input id="firstName" name="name" minlength="2" placeholder="First Name" type="text" required />
+						<input id="firstName" name="firstName" minlength="2" placeholder="First Name" type="text" required />
 					</label>
 					<label for="lastName">Last Name:
-						 <input id="lastName" name="lname"minlength="2" placeholder="Last Name" type="text" required />
+						 <input id="lastName" name="lastName"minlength="2" placeholder="Last Name" type="text" required />
 					</label>
 					<label for="email">E-Mail:
 						<input id="email" name="email" placeholder="Email" type="email" required />
@@ -21,8 +21,8 @@ include 'header.php';
 					<label for="phone">Phone:
 						<input id="phone" name="phone" placeholder="(555)555-5555" type="tel" required />
 					</label>
-					<label for="comment">Comment:
-					<textarea id="comment" name="comment" placeholder="Enter comments..." required /></textarea>
+					<label for="comments">Comment:
+					<textarea id="comments" name="comments" placeholder="Enter comments..." required /></textarea>
 					</label>
 					<input type="submit" name="submit" value="submit">
 				</div>
@@ -30,6 +30,96 @@ include 'header.php';
 		</form>
 		</div>
 	</div>
+	<?php
+	if(isset($_POST['email'])) {
+
+	    // EDIT THE 2 LINES BELOW AS REQUIRED
+	    $email_to = "miguelusaf13@gmail.com";
+	    $email_subject = "Portfolio Mail";
+
+	    function died($error) {
+	        // your error code can go here
+	        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+	        echo "These errors appear below.<br /><br />";
+	        echo $error."<br /><br />";
+	        echo "Please go back and fix these errors.<br /><br />";
+	        die();
+	    }
+
+
+	    // validation expected data exists
+	    if(!isset($_POST['first_name']) ||
+	        !isset($_POST['last_name']) ||
+	        !isset($_POST['email']) ||
+	        !isset($_POST['telephone']) ||
+	        !isset($_POST['comments'])) {
+	        died('We are sorry, but there appears to be a problem with the form you submitted.');
+	    }
+
+
+
+	    $first_name = $_POST['first_name']; // required
+	    $last_name = $_POST['last_name']; // required
+	    $email_from = $_POST['email']; // required
+	    $telephone = $_POST['phone']; // not required
+	    $comments = $_POST['comments']; // required
+
+	    $error_message = "";
+	    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+	  if(!preg_match($email_exp,$email_from)) {
+	    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+	  }
+
+	    $string_exp = "/^[A-Za-z .'-]+$/";
+
+	  if(!preg_match($string_exp,$first_name)) {
+	    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+	  }
+
+	  if(!preg_match($string_exp,$last_name)) {
+	    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+	  }
+
+	  if(strlen($comments) < 2) {
+	    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+	  }
+
+	  if(strlen($error_message) > 0) {
+	    died($error_message);
+	  }
+
+    $email_message = "Form details below.\n\n";
+
+
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+
+
+
+    $email_message .= "First Name: ".clean_string($first_name)."\n";
+    $email_message .= "Last Name: ".clean_string($last_name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Telephone: ".clean_string($telephone)."\n";
+    $email_message .= "Comments: ".clean_string($comments)."\n";
+
+	// create email headers
+	$headers = 'From: '.$email_from."\r\n".
+	'Reply-To: '.$email_from."\r\n" .
+	'X-Mailer: PHP/' . phpversion();
+	@mail($email_to, $email_subject, $email_message, $headers);
+	?>
+
+	<!-- include your own success html here -->
+
+	Thank you for contacting us. We will be in touch with you very soon.
+
+	<?php
+
+}
+	 ?>
 	<!-- //form validation jquery -->
 	<script
 		src="http://code.jquery.com/jquery-3.2.1.min.js"
